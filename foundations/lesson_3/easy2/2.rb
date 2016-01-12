016ba06c41e70f9ace948f9a09fefa4c3b4a75b6
Ryan McPherson <ryanthered@gmail.com>
2016-01-12 11:08:10 -0800

    Easy exercises

---
+++ foundations/lesson_3/easy2/2.rb
@@ -0,0 +1,6 @@
+ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 5843, "Eddie" => 10, "Marilyn" => 22, "Spot" => 237 }
+
+total = 0
+ages.each_value {|value| total += value}
+
+puts total
---
+++ foundations/lesson_3/easy2/4.rb
@@ -0,0 +1,13 @@
+munsters_description = "The Munsters are creepy in a good way."
+
+# "The munsters are creepy in a good way.""
+munsters_description.downcase.capitalize
+
+# "tHE mUNSTERS ARE CREEPY IN A GOOD WAY.""
+munsters_description.swapcase
+
+# "the munsters are creepy in a good way."
+munsters_description.downcase
+
+# "THE MUNSTERS ARE CREEPY IN A GOOD WAY."
+munsters_description.upcase
